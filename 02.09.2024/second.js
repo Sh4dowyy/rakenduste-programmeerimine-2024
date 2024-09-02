92d6d7e9969c57c56207f74dbcff027887af58bf
Sh4dowyy <dmitrii.agarjov@gmail.com>
2024-09-02 17:07:04 +0300

    feat: done 6 exercises

---
+++ 02.09.2024/second.js
@@ -0,0 +1,54 @@
+//Exercise 1. Make a list and find index of a number
+
+const list = [1, 10, 100, 1000, 10000];
+
+
+
+let position = list.indexOf(10);
+console.log(position);
+
+
+
+function findIndex(array, num) {
+    return list.indexOf(num);
+}
+console.log(findIndex(list, 10))
+
+//Exercise 2. Add 2 numbers
+
+function addNumbersFn(num1, num2){
+    return num1 + num2
+}
+console.log(addNumbersFn(3, 4))
+
+
+// Exercise 3. Remake with arrow function
+
+const addNumberArrowFn = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(addNumberArrowFn(1, 2))
+
+
+// Exercise 4. Remake with shorthand arrow function
+
+const addNumberArrowFnShort = (num1, num2) => num1 + num2;
+console.log(addNumberArrowFnShort(5, 6))
+
+
+// Exercise 5. Luua funktsioon sellisel kujul, et saaks kutsuda välja seda nii:
+
+function addNumbersNested(num1){
+    return function (num2) {
+        return num1 + num2        
+    }
+}
+console.log(addNumbersNested(3)(4))
+
+
+
+// Ex 6. Arrow function
+
+const addNumbersNestedArrow = (num1) => (num2) => num1 + num2;
+
+console.log(addNumbersNestedArrow(2)(4));
