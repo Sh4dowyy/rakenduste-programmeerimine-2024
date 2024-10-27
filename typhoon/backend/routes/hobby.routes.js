1c883454c66282bc5d262ca4e097c94133324765
Sh4dowyy <dmitrii.agarjov@gmail.com>
2024-10-27 13:11:27 +0200

    feat: validator added

--- typhoon/backend/routes/hobby.routes.js
+++ typhoon/backend/routes/hobby.routes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 const hobbiesController = require("../controllers/hobby.controller");
 const { hobbiesRouteMiddleware, hobbiesGetRouteMiddleware } = require("../middlewares/hobby.middlewares");
 const { generateToken, verifyToken } = require('../routes/jwt.routes');
+const { body, validationResult } = require('express-validator');
 
 
 router.use(hobbiesRouteMiddleware);
 
 router.get("/", hobbiesGetRouteMiddleware, hobbiesController.read);
-router.post("/", hobbiesController.create);
+router.post("/", [ body("title").notEmpty() ], hobbiesController.create);
 router.put("/", hobbiesController.update);
 router.delete("/", hobbiesController.delete);
 
